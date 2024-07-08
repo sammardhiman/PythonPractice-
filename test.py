31bf2bee2abf3c8c101ff211021ab328d2961740
sammardhiman <sand.kumar1984@gmail.com>
2024-07-08 19:04:23 +0530

    This is just a test file to test things

---
+++ test.py
@@ -0,0 +1,23 @@
+test_str = "GeeksForGeeksssss"
+new_str = test_str.replace('e', '')
+print(test_str)
+print(new_str)
+
+new_str1 = test_str.replace('s', '')
+print(new_str1)
+
+
+# def remove_ith_character(s, i):
+
+s = "GeeksForGeeks"
+# new_str = remove_ith_character(test_str, 5)
+# print("The string after removal of ith character:", new_str)
+ 
+if i == 0:
+    print(s[1:] )
+    
+print(s[0] + s(s[1:], i - 1))
+ 
+# test_str = "GeeksForGeeks"
+# new_str = remove_ith_character(test_str, 5)
+# print("The string after removal of ith character:", new_str)
